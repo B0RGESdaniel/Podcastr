3e174f0f2293595c4b51b700e8d5db1ae5e25334
Daniel Borges <dlopesborges@gmail.com>
2021-04-20 14:50:26 -0300

    Creating the SSG API
    Created the SSG API that reolads every 8 hours

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
 
-export default function Home() {
+export default function Home(props) {
+
   return (
-    <h1>Index</h1> 
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+     
   )
+}
+
+// * jeito Static Site Generation
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return { 
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // 8 horas
+    // intervalo de tempo em q a página fará novas requisições
+  }
 }
